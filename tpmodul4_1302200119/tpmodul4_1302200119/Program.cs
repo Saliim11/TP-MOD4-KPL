3e4e6ffc8dea8d06db91d75182d3a68375da8aeb
Saliim11 <qalbunsaliimbakhri@gmail.com>
2022-03-17 22:55:11 +0700

    menambahkan class DataGeneric

--- tpmodul4_1302200119/tpmodul4_1302200119/Program.cs
+++ tpmodul4_1302200119/tpmodul4_1302200119/Program.cs
@@ -10,6 +10,27 @@ namespace tpmodul4_1302200119
     {
         static void Main(string[] args)
         {
+            string data;
+            Console.Write("Masukkan data: ");
+            data = Console.ReadLine();
+
+            DataGeneric<string> data1 = new DataGeneric<string>(data);
+            data1.PrintData();
         }
     }
+
+    class DataGeneric<T>
+    {
+        T data;
+        public DataGeneric(T Data)
+        {
+            this.data = Data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + this.data);
+        }
+    }
+
 }
